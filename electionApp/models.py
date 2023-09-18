540cabe954b13056c7ea765feba833aa95e09f7a
Ashish Kumar <ashishkumar@Ashishs-MacBook-Air.local>
2023-09-19 03:06:19 +0530

    Added changes in registration from

--- electionApp/models.py
+++ electionApp/models.py
@@ -5,9 +5,9 @@ class PartyData(models.Model):
     party_id = models.AutoField(primary_key=True)
     party_name = models.CharField(max_length=255, unique=True)
     email = models.CharField(max_length=255, unique=True)
-    password = models.CharField(
-        max_length=255, null=True, blank=True, default='')
-    founder = models.CharField(
+    # password = models.CharField(
+    #     max_length=255, null=True, blank=True, default='')
+    founder_1 = models.CharField(
         max_length=255, null=True, blank=True, default='')
     founded_year = models.CharField(
         max_length=255, null=True, blank=True, default='')
@@ -19,10 +19,25 @@ class PartyData(models.Model):
         max_length=255, null=True, blank=True, default='')
     donations = models.CharField(
         max_length=255, null=True, blank=True, default='')
-    contact_details = models.TextField(null=True, blank=True, default='')
-    party_logo = models.CharField(
-        max_length=255, null=True, blank=True, default='')
-    about_me = models.TextField(null=True, blank=True, default='')
+    party_contact_details = models.TextField(null=True, blank=True, default='')
+    party_logo = models.FileField(upload_to='media/', blank=True, default='') 
+    about_party = models.TextField(null=True, blank=True, default='')
+
+    party_q_performance = models.TextField(null=True, blank=True, default='')
+    party_q_productivity= models.TextField(null=True, blank=True, default='')
+    party_q_religious = models.TextField(null=True, blank=True, default='')
+    party_q_terrorism = models.TextField(null=True, blank=True, default='')
+    party_q_internation_relation = models.TextField(null=True, blank=True, default='')
+    party_q_introduce = models.TextField(null=True, blank=True, default='')
+    party_address = models.TextField(null=True, blank=True, default='')
+    founder_2 =  models.TextField(null=True, blank=True, default='')
+    founder_3 = models.TextField(null=True, blank=True, default='')
+    founder_4 = models.TextField(null=True, blank=True, default='')
+    time_in_gov = models.TextField(null=True, blank=True, default='')
+    # poling_avg = models.TextField(null=True, blank=True, default='')
+
+
+
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField(auto_now_add=True)
@@ -49,14 +64,13 @@ class CondidateData(models.Model):
     social_media_contact = models.CharField(
         max_length=255, null=True, blank=True, default='')
     about_me = models.TextField(null=True, blank=True, default='')
-
     unique_strg = models.TextField(null=True, blank=True, default='')
     why_running = models.TextField(null=True, blank=True, default='')
     issues_facing = models.TextField(null=True, blank=True, default='')
     why_vote = models.TextField(null=True, blank=True, default='')
     cand_priority = models.TextField(null=True, blank=True, default='')
     cand_presslink = models.TextField(null=True, blank=True, default='')
-
+    cand_img = models.FileField(upload_to='candidates/', blank=True, default='')
     is_active = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now=True)
     modified_at = models.DateTimeField(auto_now_add=True)
--- electionProject/settings.py
+++ electionProject/settings.py
@@ -137,6 +137,10 @@ STATIC_URL = 'static/'
 STATIC_ROOT = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+MEDIA_URL = 'media/'
+MEDIA_ROOT = 'media/'
+MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
