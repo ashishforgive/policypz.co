9abec6cc442109f2eeba99464cccd3d439020ffc
Ashish Kumar <ashishkumar@Ashishs-MacBook-Air.local>
2023-09-19 07:15:57 +0530

    Added profile

--- electionProject/settings.py
+++ electionProject/settings.py
@@ -148,3 +148,4 @@ MEDIAFILES_DIRS = [os.path.join(BASE_DIR, 'media')]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://nzpolicy1-64b90404824a.herokuapp.com/']
